8c34b7e6e298b974a2e47327747f456791b0190a
GabrielRMuller <gabrielmuller97@gmail.com>
2020-09-10 20:38:13 -0300

    add hotels and rooms

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::resource('users', UserController::class);
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('hotels', 'HotelController');
+Route::resource('hotels.rooms', 'HotelRoomController');
+    
